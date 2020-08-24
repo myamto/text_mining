7499bb03e07c689e8eb1c06f2648713b5d0bcb65
miyamotomasato <xmasatox0530x@gmail.com>
2020-08-24 18:55:35 +0900

    20200824

--- comejisyo.py
+++ comejisyo.py
@@ -4,3 +4,15 @@ tagger = MeCab.Tagger('-d /usr/local/lib/mecab/dic/ipadic -u /usr/local/lib/meca
 text = '現在のADLは、寝返り・起き上がりは自立、立ち上がりは修正自立、移乗は介助が必要である。'
 
 print(tagger.parse(text))
+
+tikan=[]
+for line in tagger.parse(text).splitlines():
+    if "看" in line.split(",")[-1]:
+        tikan.append(line.split("\t")[0])
+
+print(tikan)
+
+for word in tikan:
+    text = text.replace(word, "ケガ")
+
+print(text)
