a40cd403788e518ca0d047a310b609e6cd3c9832
miyamotomasato <xmasatox0530x@gmail.com>
2020-09-01 20:31:04 +0900

    20200901

--- MldataGene.py
+++ MldataGene.py
@@ -6,12 +6,12 @@ import MldataGeneFunc as func
 def main():
     label_ls = []  # 初期化
     text_ls = []  # 初期化
-    f_input = open("data/taining_data.csv", 'r')  # fileのopen
+    f_input = open("data/training_data.csv", 'r')  # fileのopen
     f_output = open("mldata.csv", mode='w')  # 出力するファイルの選択
     data = csv.reader(f_input)  # ファイルの読み込み(str型)
 #------------------------------1---------------------------------
     for row in data:
-        label_ls.append(int(row[0]))  # ラベルを取得
+        label_ls.append(row[0])  # ラベルを取得
         text_ls.append(str(row[1]))  # テキストを取得
 #------------------------------2---------------------------------
     for i, text in enumerate(text_ls):
@@ -21,7 +21,7 @@ def main():
         keijoshi_average = func.keijoshi_average(text)
         kudokuten = func.kudokuten(text)
 
-        d=str(rabel_ls[i]) + "," + str(kanji_shiyoritsu) + "," + str(sentence_average_length) + "," + str(keijoshi_average) + "," + str(kudokuten)
+        d=str(label_ls[i]) + "," + str(kanji_shiyoritsu) + "," + str(sentence_average_length) + "," + str(keijoshi_average) + "," + str(kudokuten)
         f_output.write(d + "\n")
 
     f_output.close()
